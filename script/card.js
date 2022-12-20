da201536f7245bf0c0c482f15f3939ed15ca8007
Mariayar <mari.mari.sysolyatina@mail.ru>
2022-12-20 21:14:56 +0900

    Project created

---
+++ script/card.js
@@ -0,0 +1,27 @@
+class Card {
+    constructor(dataCat, selectorTemplate) {
+      this._data = dataCat;
+      this._selectorTemplate = selectorTemplate;
+    }
+  
+    _getTemplate() {
+      return document
+        .querySelector(this._selectorTemplate)
+        .content.querySelector('.card');
+    }
+    getElement() {
+      this.element = this._getTemplate().cloneNode(true);
+      const cardTitle = this.element.querySelector('.card__name');
+      const cardImage = this.element.querySelector('.card__image');
+      const cardLike = this.element.querySelector('.card__like');
+
+      if(!this._data.favorite){
+        cardLike.remove()
+      }
+      cardTitle.textContent = this._data.name;
+      cardImage.src = this._data.img_link;
+  
+      return this.element;
+    }
+  }
+  
